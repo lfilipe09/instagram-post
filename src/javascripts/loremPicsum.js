848f4b2f395f37bc88d623ee86b6562c257b74a9
lfilipe09 <81202572+lfilipe09@users.noreply.github.com>
2022-04-30 20:28:44 -0300

    feat: create initial style and html tags

---
+++ src/javascripts/loremPicsum.js
@@ -0,0 +1,35 @@
+function getImage(url){
+  let request = new XMLHttpRequest()
+  request.open("GET", url, false)
+  request.send()
+  return request.responseText
+}
+
+function createImage(imageUrl){
+  let image = document.createElement("img")
+  image.src = imageUrl
+  image.className = 'swiper-slide'
+  return image
+}
+
+function compressImageUrl(imageUrl){
+  let splittedUrl = imageUrl.split('/')
+  let formattedUrl = splittedUrl.map((parsedUrl, index) => index === 5 ? '534' : index === 6 ? '325' : parsedUrl)
+
+  let imageUrlformatted = formattedUrl.join('/')
+  return imageUrlformatted
+}
+
+function main(){
+  let data = getImage("https://picsum.photos/v2/list?page=1&limit=4")
+  let imagesData = JSON.parse(data)
+
+  imagesData.map((imageData) => {
+    let image = createImage(compressImageUrl(imageData.download_url))
+    console.log('olha a image: ', image)
+    document.getElementById('slider-images').appendChild(image)
+  })
+
+}
+
+main()
