3a1fcf1b8348120de71b9c3ffbd6c8bb3b1bdf20
lfilipe09 <81202572+lfilipe09@users.noreply.github.com>
2022-05-01 01:25:05 -0300

    feat: add heart animation

---
+++ src/javascripts/heartAnimation.js
@@ -0,0 +1,10 @@
+document.getElementById('heart-button').addEventListener('click', function (ev) {
+  console.log('executei')
+  if (document.querySelector('.heart')){
+    document.querySelector('.heart').classList.remove('heart')
+    ev.target.classList.add('heart-pulse')
+  }else{
+    document.querySelector('.heart-pulse').classList.add('heart')
+    document.querySelector('.heart-pulse').classList.remove('heart-pulse')
+  }
+});
