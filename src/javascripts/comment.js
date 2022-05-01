82c00a25b9793ff7d93aafc2bed78fbb29931250
lfilipe09 <81202572+lfilipe09@users.noreply.github.com>
2022-05-01 11:04:42 -0300

    fix: deploy fixes

--- src/javascripts/comment.js
+++ src/javascripts/comment.js
@@ -1,28 +1,32 @@
-const commentContainer = document.getElementById('allComments');
-document.getElementById('addComments').addEventListener('click', function (ev) {
+const commentContainer = document.getElementById('all-comments');
+document.getElementById('add-comments').addEventListener('click', function (ev) {
   addComment(ev);
+});
+
+document.getElementById('comment-icon').addEventListener('click', function (ev) {
+  document.querySelector('#new-comment').focus()
 });
 
 function getComments(){
   let comments = localStorage.getItem('comments')
   let commentConverted = document.createElement('div')
   commentConverted.innerHTML =  comments
-  document.getElementById('allComments').appendChild(commentConverted)
+  document.getElementById('all-comments').appendChild(commentConverted)
 }
 
 function setOnLocalStorage() {
-  localStorage.setItem('comments', document.getElementById('allComments').innerHTML);
+  localStorage.setItem('comments', document.getElementById('all-comments').innerHTML);
 }
 
 function addComment() {
-  if(document.getElementById('newComment').value){
+  if(document.getElementById('new-comment').value){
     let commentText, wrapDiv;
     const textBox = document.createElement('span');
     wrapDiv = document.createElement('div');
     wrapDiv.className = 'comment-wrapper';
     wrapDiv.style.marginLeft = 0;
-    commentText = `<b>Comment</b> ${document.getElementById('newComment').value}`;
-    document.getElementById('newComment').value = '';
+    commentText = `<b>Comment</b> ${document.getElementById('new-comment').value}`;
+    document.getElementById('new-comment').value = '';
     textBox.innerHTML = commentText;
     wrapDiv.append(textBox);
     commentContainer.appendChild(wrapDiv);
